700f687c8363dc9ca3096ece6956e137b6071ac0
brownac <austincbrown@ufl.edu>
2017-02-22 21:54:28 -0500

    certbot

--- server/main.js
+++ server/main.js
@@ -3,10 +3,15 @@ import {Sparkpost} from 'meteor/agoldman:sparkpost-mail'
 import Posts from '../imports/api/posts'
 import Images from '../imports/api/images'
 import Contacts from '../imports/api/contacts'
+import {WebApp} from 'meteor/webapp'
 
 Meteor.startup(() => {
   // code to run on server at startup
   Sparkpost.config('41f869fe72d40df7af0982193805f6c20c99915d');
+  WebApp.connectHandlers.use("/.well-known/acme-challenge/8YYSJxoKpfvB1kyXG60ARbaC6A48PUWFBw_ypc_1Q94", function(req, res, next) {
+  res.writeHead(200);
+  res.end("8YYSJxoKpfvB1kyXG60ARbaC6A48PUWFBw_ypc_1Q94.J7O86oNK4FQRgcA7o8AEwu_C00z4l5TimPoKiEg1xls");
+});
 });
 
 Meteor.publish("posts",()=>{
